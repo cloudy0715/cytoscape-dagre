574bcdc1bff3e9b13ad19d8afce1573b6f8e46e8
cloudy0715 <selina890715@gmail.com>
2022-08-10 16:50:32 +0800

    add new js

---
+++ src/navbar.js
@@ -0,0 +1,31 @@
+$(".sidebar-dropdown > a").click(function() {
+    $(".sidebar-submenu").slideUp(200);
+    if (
+      $(this)
+        .parent()
+        .hasClass("active")
+    ) {
+      $(".sidebar-dropdown").removeClass("active");
+      $(this)
+        .parent()
+        .removeClass("active");
+    } else {
+      $(".sidebar-dropdown").removeClass("active");
+      $(this)
+        .next(".sidebar-submenu")
+        .slideDown(200);
+      $(this)
+        .parent()
+        .addClass("active");
+    }
+  });
+
+  $("#close-sidebar").click(function() {
+    $(".page-wrapper").removeClass("toggled");
+  });
+  $(".sidebar-sub-wrapper #close-sidebar").click(function() {
+    $(".sidebar-sub-wrapper").removeClass("active");
+  });
+  $("#show-sidebar").click(function() {
+    $(".page-wrapper").addClass("toggled");
+  });
